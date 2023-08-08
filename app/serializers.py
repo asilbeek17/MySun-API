3656b215114ca5c7777838c0dd86656e0abfdf93
asilbek <asilbek1721@gmail.com>
2023-08-08 19:05:03 +0500

    init commit 2

--- app/serializers.py
+++ app/serializers.py
@@ -1,90 +1,65 @@
-from rest_framework import serializers
-from .models import Product_Category, Product, Project_Category, Project, Blog, Contact, Review, Review_for_business,Newsletter
+from rest_framework.serializers import ModelSerializer
+from app.models import *
 
 
-# PRODUCT #
-# PRODUCT CATEGORY SERIALIZERS
-class Product_CategorySerializers(serializers.ModelSerializer):
+class ProductCategorySerializer(ModelSerializer):
+
     class Meta:
-        fields = ('id', 'title')
         model = Product_Category
+        exclude = ()
 
 
-# PRODUCT SERIALIZERS
-class ProductSerializers(serializers.ModelSerializer):
+class ProductSerializer(ModelSerializer):
+
     class Meta:
-        fields = ('id','category','title','price','info','descriptions','image_1','image_2','image_3','image_4','characteristic',
-               )
         model = Product
-
-# END PRODUCT SERIALIZERS #
-
+        exclude = ()
 
 
+class ProjectCategorySerializer(ModelSerializer):
 
-# PROJECT #
-# PROJECT CATEGORY SERIALIZERS
-class Project_CategorySerializers(serializers.ModelSerializer):
     class Meta:
-        fields = ('id','title')
         model = Project_Category
+        exclude = ()
 
-# PROJECT SERIALIZERS
-class ProjectSerializers(serializers.ModelSerializer):
+
+class ProjectSerializer(ModelSerializer):
+
     class Meta:
-        fields = ('id','category','title','descriptions','power','annual_productivity','amount_of_savings','image_1','image_2','image_3','image_4','image_5','image_6')
         model = Project
-
-# END PROJECT SERIALIZERS #
-
+        exclude = ()
 
 
+class BlogSerializer(ModelSerializer):
 
-# BLOG SERIALIZERS #
-class BlogSerializers(serializers.ModelSerializer):
     class Meta:
-        fields = ('id','title','date','image','description')
         model = Blog
-
-# END BLOG SERIALIZERS #
-
+        exclude = ()
 
 
-# CONTACT SERIALIZERS #
-class ContactSerializers(serializers.ModelSerializer):
+class ContactSerializer(ModelSerializer):
+
     class Meta:
-        fields = ('id', 'full_name', 'email', 'phone_number', 'message')
         model = Contact
-
-# END CONTACT SERIALIZERS #
-
+        exclude = ()
 
 
+class ReviewSerializer(ModelSerializer):
 
-# REVIEW SERIALIZERS #
-class ReviewSerializers(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'full_name','phone_number', 'message')
         model = Review
-
-# END REVIEW SERIALIZERS #
-
+        exclude = ()
 
 
-# REVIEW FOR BUSINESS  SERIALIZERS #
-class Review_for_businessSerializers(serializers.ModelSerializer):
+class ReviewForBusinessSerializer(ModelSerializer):
+
     class Meta:
-        fields = ('id', 'full_name','phone_number', 'company')
         model = Review_for_business
-
-# END REVIEW FOR BUSINESS SERIALIZERS #
-
+        exclude = ()
 
 
-# NEWSLETTER SERIALIZERS #
-class NewsLetterSerializers(serializers.ModelSerializer):
+class NewsletterReviewSerializer(ModelSerializer):
+
     class Meta:
-        fields = ('id','email')
         model = Newsletter
-
-# END NEWSLETTER SERIALIZERS #
+        exclude = ()
--- app/views.py
+++ app/views.py
@@ -1,43 +1,49 @@
 from rest_framework.viewsets import ModelViewSet
 from app.models import Product_Category, Product, Project_Category, Project, Blog, Contact, Review, Review_for_business, \
     Newsletter
-from app.serializers import Product_CategorySerializers, ProductSerializers, Project_CategorySerializers, \
-    ProjectSerializers, BlogSerializers, ContactSerializers, ReviewSerializers, Review_for_businessSerializers, \
-    NewsLetterSerializers
+from app.serializers import *
 
 
 class Product_CategoryView(ModelViewSet):
     queryset = Product_Category.objects.all().order_by('-id')
-    serializer_class = Product_CategorySerializers
+    serializer_class = ProductCategorySerializer
+
 
 class ProductView(ModelViewSet):
     queryset = Product.objects.all().order_by('-id')
-    serializer_class = ProductSerializers
+    serializer_class = ProductSerializer
+
 
 class Project_CategoryView(ModelViewSet):
     queryset = Project_Category.objects.all().order_by('-id')
-    serializer_class = Project_CategorySerializers
+    serializer_class = ProjectCategorySerializer
+
 
 class ProjectView(ModelViewSet):
     queryset = Project.objects.all().order_by('-id')
-    serializer_class = ProjectSerializers
+    serializer_class = ProjectSerializer
+
 
 class BlogView(ModelViewSet):
     queryset = Blog.objects.all().order_by('-id')
-    serializer_class = BlogSerializers
+    serializer_class = BlogSerializer
+
 
 class ContactView(ModelViewSet):
     queryset = Contact.objects.all().order_by('-id')
-    serializer_class = ContactSerializers
+    serializer_class = ContactSerializer
+
 
 class ReviewView(ModelViewSet):
     queryset = Review.objects.all().order_by('-id')
-    serializer_class = ReviewSerializers
+    serializer_class = ReviewSerializer
+
 
 class Review_for_businessView(ModelViewSet):
     queryset = Review_for_business.objects.all().order_by('-id')
-    serializer_class = Review_for_businessSerializers
+    serializer_class = ReviewForBusinessSerializer
+
 
 class NewsletterView(ModelViewSet):
     queryset = Newsletter.objects.all().order_by('-id')
-    serializer_class = NewsLetterSerializers
+    serializer_class = NewsletterReviewSerializer
